cbabf6c6947984c2f68e2af5c5e077d91edb7a46
agustin mateos <agustinrotondomateos@gmail.com>
2025-06-19 01:44:09 -0300

    page

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -21,7 +21,7 @@ export default async function Home({ params: { locale } }) {
       {/* Segundo componente: Sección con imagen */}
       <div className="h-auto md:h-[780px] w-full flex flex-col md:flex-row items-center">
         <div className="w-[100%] md:w-[50%] h-full flex items-center justify-center">
-          <div className="h-[700px] md:h-[600px] flex justify-center md:justify-around flex-col items-center w-[80%] md:w-[480px]">
+          <div className="h-[700px] md:h-[600px] flex justify-around flex-col items-center w-[80%] md:w-[480px]">
             <div className="h-[427px] w-[240px] relative">
               <Image src='/espacios.svg'
               alt="espacios"
@@ -29,7 +29,7 @@ export default async function Home({ params: { locale } }) {
 
               />
             </div>
-            <div className="flex h-[150px] md:h-[107] flex-col justify-end md:justify-end">
+            <div className="flex h-[160px] md:h-[107] flex-col justify-end md:justify-end">
               <p className={`${inter.className} font-medium text-[28px] leading-[110%] tracking-[0]`}>Diseñamos espacios que combinan funcionalidad, estética y calidad.</p>
               <div className="flex pt-[20px] "><p className={`${roboto.className} font-medium text-[14px] leading-[110%] tracking-[10%] `} >QUIÉNES SOMOS</p><Image width={15} height={15} alt="img" src='/line.svg'/></div>
               </div>
@@ -63,7 +63,7 @@ export default async function Home({ params: { locale } }) {
               fill
               />
             </div>
-            <div className="flex h-[201px] md:h-[123px] flex-col justify-end md:justify-end">
+            <div className="flex h-auto md:h-[123px] flex-col justify-end md:justify-end">
               <p className={`${inter.className} font-medium  text-[28px] leading-[110%] tracking-[0]`}>Cada espacio que creamos es una interpretación de lo que nuestros clientes necesitan y aún no saben que desean. </p>
               <div className="flex pt-[20px] "><p className={`${roboto.className} font-medium text-[14px] leading-[110%] tracking-[10%] `} >TRABAJEMOS JUNTOS</p><Image width={15} height={15} alt="img" src='/line.svg'/></div>
               </div>
@@ -72,9 +72,9 @@ export default async function Home({ params: { locale } }) {
         </div>
         
       </div>
-      <div className="w-full h-[120px] flex flex-col  justify-center bg-[#A28D77]">
-        <p className={`${inter.className} w-[90%] pl-[60px] pt-[20px] font-normal text-[24px] leading-[120%] tracking-[0]`}>«La arquitectura debe hablar de su tiempo y lugar, pero aspirar a la atemporalidad».</p>
-        <p className={`${inter.className} w-[90%]  pl-[60px] font-normal text-[24px] leading-[120%] tracking-[0]`}>
+      <div className="w-full h-[176px] p-[20px] md:p-[0px] md:h-[120px] flex flex-col  justify-center bg-[#A28D77]">
+        <p className={`${inter.className} w-[90%] md:pl-[60px] md:pt-[20px] font-normal text-[24px] leading-[120%] tracking-[0]`}>«La arquitectura debe hablar de su tiempo y lugar, pero aspirar a la atemporalidad».</p>
+        <p className={`${inter.className} w-[90%]  md:pl-[60px] font-normal text-[24px] leading-[120%] tracking-[0]`}>
         — Frank Gehry  </p>
         </div>
     </div>
