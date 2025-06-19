1dd3982a3c5c3273e34e7c4777231bd135d9fdfd
agustin mateos <agustinrotondomateos@gmail.com>
2025-06-18 23:30:10 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Footer = () => {
     return (
         <div className='w-full bg-[#46241E] p-[30px] h-auto md:h-[180px] flex flex-col md:flex-row items-center justify-between'>
-            <div className='h-[140px] w-full md:w-[470px] justify-around flex flex-col'>
+            <div className='h-auto md:h-[140px] w-full md:w-[470px] justify-around flex flex-col'>
 
                 <div className='h-[74px]  w-full'>
                     <Image
@@ -21,7 +21,7 @@ const Footer = () => {
                 <div><p>nosotros</p></div>
                 <div><p>contactos</p></div>
             </div>
-            <div className='w-full md:w-[112px] pt-[20px] pb-[10xp] md:pt-[0px] md:pb-[0xp]   h-[28px] md:h-full flex justify-around '>
+            <div className='w-full md:w-[112px] pt-[20px] pb-[10px] md:pt-[0px] md:pb-[0xp]   h-auto md:h-full flex justify-around '>
                 <div className='h-[24px] relative  w-[24px]'>
                     <Image
                         fill
@@ -44,7 +44,7 @@ const Footer = () => {
                         className="object-contain"
                     /></div>
             </div>
-            <div className='h-auto md:h-[17px] w-full block md:hidden pt-[10px]'><p className='text-[#EAE4D6] text-[14px]'>© 2025 DYB Arqs. Todos los derechos reservados. </p></div>
+            <div className='h-auto md:h-[17px] w-full block md:hidden pt-[10px]'><p className='text-[#EAE4D6] text-[14px] text-center '>© 2025 DYB Arqs. Todos los derechos reservados. </p></div>
         </div>
     )
 }
