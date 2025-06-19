f857db63079500a448f2377236c0cd275226554c
agustin mateos <agustinrotondomateos@gmail.com>
2025-06-19 01:34:10 -0300

    roboto

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import initTranslations from "../i18n";
 import Link from "next/link";
-import { inter } from "./ui/fonts";
+import { inter, roboto } from "./ui/fonts";
 export default async function Home({ params: { locale } }) {
   const { t } = await initTranslations(locale, ["home", "common"]);
   return (
@@ -30,8 +30,8 @@ export default async function Home({ params: { locale } }) {
               />
             </div>
             <div className="flex h-[150px] md:h-[107] flex-col justify-end md:justify-end">
-              <p className={`${inter.className} `}>Diseñamos espacios que combinan funcionalidad, estética y calidad.</p>
-              <div className="flex pt-[20px] "><p className={`${inter.className} `} >QUIÉNES SOMOS</p><Image width={15} height={15} alt="img" src='/line.svg'/></div>
+              <p className={`${inter.className} font-medium text-[28px] leading-[110%] tracking-[0]`}>Diseñamos espacios que combinan funcionalidad, estética y calidad.</p>
+              <div className="flex pt-[20px] "><p className={`${roboto.className} font-medium text-[14px] leading-[110%] tracking-[10%] `} >QUIÉNES SOMOS</p><Image width={15} height={15} alt="img" src='/line.svg'/></div>
               </div>
              
           </div>
@@ -56,16 +56,16 @@ export default async function Home({ params: { locale } }) {
           />
         </div> 
         <div className="w-[100%] md:w-[50%] h-full flex items-center justify-center">
-          <div className="h-[700px] md:h-[600px] flex justify-center md:justify-around flex-col items-center w-[80%] md:w-[480px]">
+          <div className="h-[800px] md:h-[800px] flex justify-center md:justify-around flex-col items-center w-[80%] md:w-[480px]">
             <div className="h-[480px] w-[309px] md:w-[438px] relative">
               <Image src='/22alfrente.svg'
               alt="espacios"
               fill
               />
             </div>
-            <div className="flex h-[150px] md:h-[107] flex-col justify-end md:justify-end">
-              <p className={`${inter.className} `}>Cada espacio que creamos es una interpretación de lo que nuestros clientes necesitan y aún no saben que desean. </p>
-              <div className="flex pt-[20px] "><p className={`${inter.className} `} >TRABAJEMOS JUNTOS</p><Image width={15} height={15} alt="img" src='/line.svg'/></div>
+            <div className="flex h-[201px] md:h-[123px] flex-col justify-end md:justify-end">
+              <p className={`${inter.className} font-medium  text-[28px] leading-[110%] tracking-[0]`}>Cada espacio que creamos es una interpretación de lo que nuestros clientes necesitan y aún no saben que desean. </p>
+              <div className="flex pt-[20px] "><p className={`${roboto.className} font-medium text-[14px] leading-[110%] tracking-[10%] `} >TRABAJEMOS JUNTOS</p><Image width={15} height={15} alt="img" src='/line.svg'/></div>
               </div>
              
           </div>
@@ -73,8 +73,8 @@ export default async function Home({ params: { locale } }) {
         
       </div>
       <div className="w-full h-[120px] flex flex-col  justify-center bg-[#A28D77]">
-        <p className={`${inter.className} w-[90%] pl-[60px] pt-[20px]`}>«La arquitectura debe hablar de su tiempo y lugar, pero aspirar a la atemporalidad».</p>
-        <p className={`${inter.className} w-[90%]  pl-[60px]`}>
+        <p className={`${inter.className} w-[90%] pl-[60px] pt-[20px] font-normal text-[24px] leading-[120%] tracking-[0]`}>«La arquitectura debe hablar de su tiempo y lugar, pero aspirar a la atemporalidad».</p>
+        <p className={`${inter.className} w-[90%]  pl-[60px] font-normal text-[24px] leading-[120%] tracking-[0]`}>
         — Frank Gehry  </p>
         </div>
     </div>
--- src/app/[locale]/ui/fonts.jsx
+++ src/app/[locale]/ui/fonts.jsx
@@ -1,6 +1,11 @@
 import { Inter_Tight } from "next/font/google";
-
+import { Roboto_Mono } from "next/font/google";
 export const inter = Inter_Tight({ 
   subsets: ["latin"], 
   weight: ["100", "200", "300", "400", "500", "700"] // Asegúrate de que estos pesos existan para Inter Tight
 });
+
+export const roboto = Roboto_Mono({ 
+    subsets: ["latin"], 
+    weight: ["100", "200", "300", "400", "500", "700"] // Asegúrate de que estos pesos existan para Inter Tight
+  });
